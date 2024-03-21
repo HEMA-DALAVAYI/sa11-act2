aa6ae3d0c0c8ba3d372c15981a24cf1e9a86a6ad
Hema Dalavayi <gayathri.dhs@gmail.com>
2024-03-20 21:51:00 -0500

    Correctly extracted all the hastags

---
+++ prob1.rb
@@ -0,0 +1,10 @@
+'''
+1.	Extracting Hashtags from a Tweet Replace nil with the regular expression to extract all hashtags.
+'''
+def extract_hashtags(text)
+  pattern = /([#]\D+?\b)/
+  hashtags = text.scan(pattern)
+  hashtags.flatten
+end
+
+puts extract_hashtags("Love this #beautiful day! #sunny")

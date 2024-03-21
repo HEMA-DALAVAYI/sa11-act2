43ed09d0440d4547580bee233fb7c8b51829983e
Hema Dalavayi <gayathri.dhs@gmail.com>
2024-03-20 21:52:01 -0500

    Was correctly able to get all the numerical values

---
+++ prob3.rb
@@ -0,0 +1,11 @@
+'''
+Finding Numeric Values in a String Replace nil with the regex to find all
+numeric values in a string.
+'''
+
+def find_numbers(text)
+  pattern = /(\d+[.]\d+)/
+  text.scan(pattern)
+  end
+  puts find_numbers("The order was placed for 100.25 units at a
+  price of 20.75 each.")
